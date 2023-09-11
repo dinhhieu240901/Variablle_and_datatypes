72e01f8cda89593774da834f8456f771c83d3e5c
dinh <hieun4587@gmail.com>
2023-09-11 13:07:05 +0700

    use of operators

---
+++ Sử dụng toán tử/Use_of_operators.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt("Enter the width");
+inputHeight = prompt("Enter the Heght");
+width = parseInt(inputWidth);
+height = parseInt(inputHeight);
+let area = width * height;
+document.write("area = " + area);
